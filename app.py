a4863729458ef33dc819c4a962a4294bb3733f57
Hongfei Dong <v@ionichina.com>
2015-09-26 01:57:40 +0800

    four

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
 
 @app.route('/api/v1/music', methods=['GET'])
 def signin_form():
-    return '{"picUrl":"http://p4.music.126.net/boJMW-80CcGhrPIG7Nsaow==/82463372095702.jpg","url":"http://m2.music.126.net/li9U8WUTYs0MityAeW9zsQ==/1130297953361132.mp3"}'
+    return '{"picUrl":"http://p3.music.126.net/tOhom1sERa_KgoA78Pqa1Q==/2423323627633522.jpg","url":"http://m2.music.126.net/JNMhi2SCp9ief3mHmKqMjA==/3294136836885992.mp3"}'
 
 if __name__ == '__main__':
     app.run()
