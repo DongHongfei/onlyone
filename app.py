f19222c6204d0c21c1d4e9928d41c995888e6181
Hongfei Dong <v@ionichina.com>
2015-09-26 01:54:09 +0800

    thir

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return '{"picUrl":"http://p4.music.126.net/boJMW-80CcGhrPIG7Nsaow==/82463372095702.jpg?param=34x34","url":"http://m2.music.126.net/li9U8WUTYs0MityAeW9zsQ==/1130297953361132.mp3"}'
+    return 'home'
 
 @app.route('/api/v1/music', methods=['GET'])
 def signin_form():
-    return '{"picUrl":"http://p4.music.126.net/boJMW-80CcGhrPIG7Nsaow==/82463372095702.jpg?param=34x34","url":"http://m2.music.126.net/li9U8WUTYs0MityAeW9zsQ==/1130297953361132.mp3"}'
+    return '{"picUrl":"http://p4.music.126.net/boJMW-80CcGhrPIG7Nsaow==/82463372095702.jpg","url":"http://m2.music.126.net/li9U8WUTYs0MityAeW9zsQ==/1130297953361132.mp3"}'
 
 if __name__ == '__main__':
     app.run()
