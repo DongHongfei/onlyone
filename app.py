45b994af1acb29ec5349a0001932b0fee28eb470
Hongfei Dong <v@ionichina.com>
2015-09-27 06:54:04 +0800

    heart broken

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
 
 @app.route('/api/v1/music', methods=['GET'])
 def signin_form():
-    return '{"picUrl":"http://p3.music.126.net/tOhom1sERa_KgoA78Pqa1Q==/2423323627633522.jpg","url":"http://m2.music.126.net/JNMhi2SCp9ief3mHmKqMjA==/3294136836885992.mp3"}'
+    return '{"picUrl":"http://p3.music.126.net/ugqgjlAdvXPgX4FGFo2eXw==/75866302334121.jpg","url":"http://m2.music.126.net/oZn_gq3WK9amlixhhk1l7g==/1213860837072515.mp3"}'
 
 if __name__ == '__main__':
     app.run()
